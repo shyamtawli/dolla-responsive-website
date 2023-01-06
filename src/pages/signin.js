958497ab3bc311a17cc43cc49316521375f705ed
erfanasadi <erfan.asadi2272@gmail.com>
2023-01-06 17:36:16 +0330

    add signin

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,23 @@
-import React from 'react'
+
+import React, {useState} from "react";
 
 const Signin = () => {
+    const [email,setEmail]= useState("");
+    const [password,setPassword]= useState("")
+    const handleSubmit= (e) => {
+        e.preventDefault();
+        console.log(email);
+    }
   return (
-    <div>
-        <h1>Sign In</h1>
+    <div className={"register-form"}>
+    <form className={"form-container"} onSubmit={handleSubmit}>
+        <label className={"label"} for="email">Email:</label>
+        <input className={"text-input"} value={email} type={"email"} id = "Email" placeholder="youremail@gmail.com" name="email"/>
+        <label className={"label"} htmlFor="password">Password:</label>
+        <input className={"text-input"} value={password} type={"password"} id="password" placeholder="*******" name="password"/>
+        <button className={"btn"} type={"submit"}> log in</button>
+        <div>Don't hava an account?</div> <a href="https://somthing.com">register here</a>
+    </form>
     </div>
   )
 }
